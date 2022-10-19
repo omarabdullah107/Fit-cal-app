1810b05eeebbf7492a450cd964aba3ba0ab3c973
omarabdullah107 <mromar502@hotmail.com>
2022-10-20 00:35:36 +0200

    pushing new updates with clean code

--- fit.js
+++ fit.js
@@ -1,37 +1,16 @@
 const bmi = require("@omarabdullah107/bmi");
-// simple app tp calculate the BMI of a person and shows the condition of ur health
 
-function cal(){
-    
-    resbmi = bmi(70,180);
-    return resbmi;
+const BMI = bmi(100,120)
 
+const condition = () => {  
+  if(BMI<=0) return "put right numbers"
+  if(BMI<18.5) return "You are under weight";
+  if(18.5<=BMI && BMI<=24.9) return "Normal";
+  if(25<=BMI && BMI<=29.9) return "over weight";
+  if(30<=BMI && BMI<=34.9) return "obesity(class1)";
+  if(35<=BMI && BMI<=39.9) return "obesity(class2)";
+  else return "extreme obesity";
 }
 
-
-function condition(){
-
-    if(resbmi<18.5){
-        return "You are under weight";
-    }
-    if(18.5<=resbmi<=24.9){
-        return "Normal";
-    }
-    if(25<=resbmi<=29.9){
-        return "over weight";
-    }
-    if(30<=resbmi<=34.9){
-        return "obesity(class1)";
-    }
-    if(35<=resbmi<=39.9){
-        return "obesity(class2)";
-    }
-    if(resbmi>40){
-        return "extreme obesity";
-    }
-
-}
-
-console.log("Your BMI = " + cal() + " " + "your condition is" + " " + condition());
-module.exports = cal ;
+console.log (`Your BMI = ${BMI} your condition is ${condition()}`);
 
